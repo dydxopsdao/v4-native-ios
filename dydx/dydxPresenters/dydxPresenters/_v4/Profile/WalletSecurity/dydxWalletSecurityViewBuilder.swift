93a7450034d510782fa774363d05e78870a0b318
Rui <102453770+ruixhuang@users.noreply.github.com>
2025-08-14 20:05:00 +0800

    Account screen for embedded wallet (#441)
    * Account view
    
    * Export view
    
    * Routing to export

--- dydx/dydxPresenters/dydxPresenters/_v4/Profile/WalletSecurity/dydxWalletSecurityViewBuilder.swift
+++ dydx/dydxPresenters/dydxPresenters/_v4/Profile/WalletSecurity/dydxWalletSecurityViewBuilder.swift
@@ -11,12 +11,14 @@ import PlatformParticles
 import RoutingKit
 import ParticlesKit
 import PlatformUI
+import dydxStateManager
+import Combine
 
 public class dydxWalletSecurityViewBuilder: NSObject, ObjectBuilderProtocol {
     public func build<T>() -> T? {
         let presenter = dydxWalletSecurityViewPresenter()
         let view = presenter.viewModel?.createView() ?? PlatformViewModel().createView()
-        return dydxWalletSecurityViewController(presenter: presenter, view: view, configuration: .default) as? T
+        return dydxWalletSecurityViewController(presenter: presenter, view: view, configuration: .fullScreenSheet) as? T
     }
 }
 
@@ -27,6 +29,18 @@ private class dydxWalletSecurityViewController: HostingViewController<PlatformVi
         }
         return false
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if let presenter = presenter as? dydxWalletSecurityViewPresenter {
+            presenter.isPushed = isPushed
+        }
+    }
+
+    private var isPushed: Bool {
+        navigationController?.viewControllers.firstIndex(of: self) ?? 0 > 0
+    }
 }
 
 private protocol dydxWalletSecurityViewPresenterProtocol: HostedViewPresenterProtocol {
@@ -34,9 +48,58 @@ private protocol dydxWalletSecurityViewPresenterProtocol: HostedViewPresenterPro
 }
 
 private class dydxWalletSecurityViewPresenter: HostedViewPresenter<dydxWalletSecurityViewModel>, dydxWalletSecurityViewPresenterProtocol {
+    var isPushed: Bool = true {
+        didSet {
+            viewModel?.showBackbutton = isPushed
+        }
+    }
+
     override init() {
         super.init()
 
         viewModel = dydxWalletSecurityViewModel()
+        viewModel?.cancelAction = { [weak self] in
+            self?.navigate(to: RoutingRequest(path: "/action/dismiss"), animated: true, completion: nil)
+        }
+        viewModel?.loginAction = { [weak self] in
+            self?.navigate(to: RoutingRequest(path: "/onboard/turnkey"), animated: true, completion: nil)
+        }
+    }
+
+    override func start() {
+        super.start()
+
+        AbacusStateManager.shared.state.currentWallet
+            .sink { [weak self] wallet in
+                guard let wallet = wallet else {
+                    return
+                }
+                self?.updateViewModel(wallet: wallet)
+            }
+            .store(in: &subscriptions)
+    }
+
+    private func updateViewModel(wallet: dydxWalletInstance) {
+        viewModel?.email = wallet.userEmail
+        if let loginMethod = wallet.loginMethod {
+            viewModel?.loginMethod = dydxWalletSecurityViewModel.LoginMethod(rawValue: loginMethod) ?? .email
+        }
+        viewModel?.sourceAddress = wallet.ethereumAddress
+        viewModel?.dydxAddress = wallet.cosmoAddress
+        viewModel?.exportSourceAction = { [weak self] in
+            guard let mnemonic = wallet.mnemonic else {
+                return
+            }
+            let params = ["mnemonic": mnemonic]
+            self?.navigate(to: RoutingRequest(path: "/my-profile/keyexport", params: params as [String: Any]), animated: true, completion: nil)
+        }
+
+        viewModel?.exportDydxAction = { [weak self] in
+            guard let mnemonic = wallet.sourceWalletMnemonic else {
+                return
+            }
+            let params = ["mnemonic": mnemonic]
+            self?.navigate(to: RoutingRequest(path: "/my-profile/keyexport", params: params as [String: Any]), animated: true, completion: nil)
+        }
     }
 }
